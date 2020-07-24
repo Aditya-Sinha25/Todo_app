f48455d862ba06c5fa030d6c241ad0aaaf697080
Aditya <adityagodofgamer.1@gmail.com>
2020-07-24 13:16:04 +0530

    COnnection to database established

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose =require('mongoose');
+mongoose.connect('mongodb://localhost/todo_app');
+
+const db =mongoose.connection;
+db.on('error',console.error.bind(console,'Error in connecting to the MongoDB'));
+
+db.once('open',function(){
+    console.log('Connected to the database:: MongoDB');
+});
+
+module.exports =db;
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose =require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    description:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:date,
+        required:true
+    },
+    type:{
+        type:String
+    }
+},{
+    timestamps:true
+});
+
+const List =mongoose.model('List',userSchema);
+module.exports =List;
