947e4fe082b48611005f6fe2494fd9e36fd2dfb1
Aditya <adityagodofgamer.1@gmail.com>
2020-07-24 12:42:12 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express =require('express');
+const app =express();
+const port=2000;
+
+//use express router
+app.use('/',require('./routes/index'));
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//listen to the port
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is up and running on port:${port}`);
+}); 
