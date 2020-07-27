1aa5f946aa6c49280ffab29669c8f67269300805
Aditya <adityagodofgamer.1@gmail.com>
2020-07-27 13:02:21 +0530

    Fixed some bugs

--- controllers/home.js
+++ controllers/home.js
@@ -1,14 +1,10 @@
 const List =require('../models/user');
 
 module.exports.home=function(req,res){
-    var users=List.find({},function(err,docs){
-        if(err){
-            console.log('Error showing data',err);
-            return;
-        }
-        res.render('home',{
+    List.find(function(err,list){
+        return res.render('home',{
             title:'TODO APP',
-            docs:List
+            doc:list
         });
     });
 }
